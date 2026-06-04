4804326ad8f0c01f4b2d9038a984d8f90b344a84
harsh <harshwardhany87@gmail.com>
2026-06-04 22:05:06 +0530

    make cookie secure by using secure true accept only https

--- server/src/Controllers/auth.controller.ts
+++ server/src/Controllers/auth.controller.ts
@@ -110,8 +110,8 @@ try{
     const token=jwt.sign({userId:oldUser._id,email:email,role:oldUser.role},JWT_SECRET as string);
     res.cookie('token',token,{
         httpOnly:true,
-        sameSite:'lax',
-        secure:false,
+        sameSite:'none',
+        secure:true,
         maxAge:7*24*60*60*1000,
     });
 
