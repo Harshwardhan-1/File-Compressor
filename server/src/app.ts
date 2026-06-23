27065e717cee794141998f71452fa8cd67fb25e7
Harshwardhan-1 <harshwardhany87@gmail.com>
2026-06-23 12:51:26 +0530

    Fix formatting of CORS origin in app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-  origin:FRONTEND_URL,
+  origin:FRONTEND_URL,       
   methods:['GET','POST','PUT','DELETE'],
   credentials:true,
 }));
